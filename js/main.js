e61e37dcbe3110ed97c2e96a0f643f19f155f8e5
fransan <fransantillan366@gmail.com>
2024-02-27 13:23:31 -0300

    agregmos funciones par dias ocupados

--- js/main.js
+++ js/main.js
@@ -1,40 +1,62 @@
-const validarDiaEnMes = (mes) => {
+
+const validarDiaOcupado = (diasOcupadosPorMes, mesSeleccionado, diaSeleccionado) => {
+    diaSeleccionado = parseInt(diaSeleccionado);
+
+    
+    if (mesSeleccionado in diasOcupadosPorMes) {
+       
+        if (diasOcupadosPorMes[mesSeleccionado].includes(diaSeleccionado)) {
+            return true; 
+        }
+    }
+
+    return false; 
+}
+
+const validarDiaEnMes = (mes, diasOcupadosPorMes) => {
     let dia = prompt("Ingrese el día: ");
 
-    // Validar días según el mes
     while ((mes == 2 && (dia > 28 || dia < 1)) ||
         ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia > 30 || dia < 1)) ||
-        ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia > 31 || dia < 1))) {
-        dia = prompt(`Ese día no existe en el mes ${mes}, ingrese correctamente el día: `);
+        ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia > 31 || dia < 1)) ||
+        validarDiaOcupado(diasOcupadosPorMes, mes, dia)) {
+        dia = prompt(`Ese día no está disponible en el mes  o ya está ocupado, ingrese correctamente el día: `);
     }
 
     return dia;
 }
 
-let mes = prompt("Ingrese el mes: ");
+let cliente = {};
+cliente.nombre = prompt("Por favor, ingresa tu nombre:");
+cliente.mes = prompt("Por favor, ingresa el mes en que deseas tu turno");
 
-while (mes > 12 || mes < 1) {
-    mes = prompt("Mes no válido, ingrese correctamente el mes: ");
+while (cliente.mes > 12 || cliente.mes < 1) {
+    cliente.mes = prompt("Mes no válido, ingrese correctamente el mes: ");
 }
 
-let diaEnMes = validarDiaEnMes(mes); // Se asigna el resultado de la función a diaEnMes
+let diasOcupadosPorMes = {
+    1: [5, 10], // Ejemplo: Enero, días 5 y 10 ocupados
+    2: [15, 20], // Ejemplo: Febrero, días 15 y 20 ocupados
+   
+};
 
-let horario = prompt("Ingrese su horario mañana/tarde: ");
-while (horario !== "mañana" && horario !== "tarde") {
-    horario = prompt("Ingrese un horario válido (mañana/tarde): ");
+cliente.dia = validarDiaEnMes(cliente.mes, diasOcupadosPorMes);
+
+cliente.horario = prompt("Ingrese su horario mañana/tarde: ");
+while (cliente.horario !== "mañana" && cliente.horario !== "tarde") {
+    cliente.horario = prompt("Ingrese un horario válido (mañana/tarde): ");
 }
 
-let hora = prompt("Ingrese la hora: ");
+cliente.hora = prompt("Ingrese la hora: ");
 
-if (horario === "mañana") {
-    while (hora > 12 || hora < 7) {
-        hora = prompt("Error, el horario de mañana es de 7am a 12pm. Ingrese un horario válido: ");
+if (cliente.horario === "mañana") {
+    while (cliente.hora > 12 || cliente.hora < 7) {
+        cliente.hora = prompt("Error, el horario de mañana es de 7am a 12pm. Ingrese un horario válido: ");
+    }
+} else if (cliente.horario === "tarde") {
+    while (cliente.hora > 9 || cliente.hora < 5) {
+        cliente.hora = prompt("Error, el horario de tarde es de 5pm a 9pm. Ingrese un horario válido: ");
     }
 }
-if(horario === "tarde"){
-    while (hora > 9 || hora < 5) {
-        hora = prompt("Error, el horario de tarde es de 5pm a 9pm. Ingrese un horario válido: ");
-    }
-}
 
-alert("Su turno es el día " + diaEnMes + " del mes " + mes + " a las " + hora + " de la " + horario);
+alert(cliente.nombre + " tu turno es el día " + cliente.dia + " del mes " + cliente.mes + " a las " + cliente.hora + " de la " + cliente.horario);
